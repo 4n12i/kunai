2c20f980be679af7e75512902e350517291f7ea7
4n12i <an12inakamu12a@gmail.com>
2024-08-13 11:08:15 +0900

    wip

--- kunai-ebpf/src/main.rs
+++ kunai-ebpf/src/main.rs
@@ -5,6 +5,12 @@
 // bringing probes into main
 mod probes;
 
+#[allow(non_upper_case_globals)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+#[allow(dead_code)]
+mod vmlinux;
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     unsafe { core::hint::unreachable_unchecked() }
--- kunai-ebpf/src/probes/lsm.rs
+++ kunai-ebpf/src/probes/lsm.rs
@@ -1,8 +1,21 @@
-use core::ffi::c_void;
+use core::ffi::{c_void, c_long, c_char};
 
-use aya_ebpf::{cty::c_int, programs::LsmContext};
+use aya_ebpf::{bindings::path, bpf_printk, cty::c_int, maps::PerCpuArray, programs::LsmContext};
+
+use crate::vmlinux::file;
 
 use super::*;
+
+// use vmlinux::file;
+
+
+#[repr(C)]
+struct Path {
+    path: [u8; MAX_PATH_LEN]
+}
+
+#[map]
+static mut PATH_BUF: PerCpuArray<Path> = PerCpuArray::with_max_entries(1, 0);
 
 enum LsmStatus {
     Continue(i32),
@@ -17,6 +30,53 @@ impl From<LsmStatus> for i32 {
             LsmStatus::Continue(ret) => ret,
         }
     }
+}
+
+#[lsm(hook = "file_open")]
+pub fn lsm_file_open(ctx: LsmContext) -> i32 {
+    match unsafe { try_lsm_security_file_open(&ctx) } {
+        Ok(s) => s.into(),
+        Err(s) => {
+            error!(&ctx, s);
+            // we don't block on error to prevent DOS
+            0
+        }
+    }
+}
+
+#[inline(always)]
+pub fn my_bpf_d_path(path: *mut path, buf: &mut [u8]) -> Result<usize, c_long> {
+    let ret = unsafe { aya_ebpf::helpers::bpf_d_path(path, buf.as_mut_ptr() as *mut c_char, buf.len() as u32) };
+    if ret < 0 {
+        return Err(ret);
+    }
+
+    Ok(ret as usize)
+}
+
+#[inline(always)]
+unsafe fn try_lsm_security_file_open(ctx: &LsmContext) -> Result<LsmStatus, ProbeError> {
+    let buf = unsafe {
+        let buf_ptr = PATH_BUF.get_ptr_mut(0).ok_or(0).unwrap();
+        &mut *buf_ptr
+    };
+
+    let p = {
+        let f: *const file = ctx.arg(0);
+        bpf_printk!(b"file_open: file: %s", f);
+        let p = unsafe { &(*f).f_path as *const _ as *mut path };
+        let len = my_bpf_d_path(p, &mut buf.path).map_err(|_| 0).unwrap();
+        if len >= 64 { // path_len = 64
+            return Ok(LsmStatus::Block);
+        }
+        core::str::from_utf8_unchecked(&buf.path[..len])
+    };
+
+    if p.starts_with("/.dockerenv") {
+        return Ok(LsmStatus::Block)
+    }
+
+    Ok(LsmStatus::Continue(0))
 }
 
 #[lsm(hook = "task_kill")]
